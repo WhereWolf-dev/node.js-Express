398266d15962f479eaac9e671518e6fda4288ae6
WhereWolf-dev <mongmongbaa@gmail.com>
2023-01-09 22:22:02 +0700

    index

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
+
+
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
+
 
 app.get("/", (req, res) => {
-
-    res.send('Hello Mongbaa -****99');
-
+    res.send('Hello Mongbaa ****');
 })
 
 app.listen(port, () => {
